a1657a033ecf6a9ba0db604eae7c2fb45cc8e59a
Clément <clement.delord@epitech.eu>
2022-11-02 16:31:50 +0100

    correction ex 9

--- Exercices/Exercice 9/filtre.js
+++ Exercices/Exercice 9/filtre.js
@@ -3,10 +3,20 @@ let search = document.getElementById('search');
 // search.value;
 
 search.addEventListener('keydown', function(event) {
-    if ("la touche c'est bien entrée") {
+    if (event.key == 'Enter') {
         // Récupération des éléments de la liste
+        let imageList = document.getElementsByTagName('img');
+
         // Iteration sur les images
+        for (let image of imageList) {
             // Vérification et comparaison des valeurs du champ texte et de 
             // l'attribut alt de l'image
+            if (image.getAttribute('Alt').includes(search.value)) {
+                image.style.display = 'inline';
+            }
+            else {
+                image.style.display = 'none';
+            }
+        }
     }
 });
