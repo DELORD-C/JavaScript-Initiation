3296fbdd8fb6d5c98faa6dc6d5d0d2ccf6a076e9
Clément <clement.delord@epitech.eu>
2022-11-02 13:36:19 +0100

    correction ex 4

---
+++ Exercices/Exercice 4/correction/script.js
@@ -0,0 +1,9 @@
+function chaineDeCharactere(x) {
+    let chaine = "";
+    for (let i = 0; i < x; i++) {
+        chaine = chaine + 8;
+    }
+    return chaine;
+}
+
+console.log(chaineDeCharactere(100));
