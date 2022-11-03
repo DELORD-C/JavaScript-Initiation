156fd2fdf95ac137cf9018e8814f84146167b27d
Clément <clement.delord@epitech.eu>
2022-11-03 16:45:51 +0100

    Drag And Drop

--- Exercices/Exercice 12/script.js
+++ Exercices/Exercice 12/script.js
@@ -1,4 +1,38 @@
-// eventListeners
-// drag
-// dragstart
-// dragend
+let tempLi = document.createElement('li');
+tempLi.style.listStyle = 'none';
+tempLi.style.height = '20px';
+tempLi.style.display = 'none';
+document.querySelector('ul').append(tempLi);
+let elements = document.querySelectorAll('li');
+let dragElem;
+let lastDragElem;
+
+elements.forEach(element => {
+    element.addEventListener('dragstart', function(e) {
+        tempLi.style.display = 'list-item';
+        dragElem = e.target;
+        dragElem.style.opacity = '0';
+        dragElem.style.height = '0px';
+    });
+
+    element.addEventListener('dragover', function(e) {
+        lastDragElem = e.target;
+        e.target.style.borderTop = "solid 10px gray";
+    });
+
+    element.addEventListener('dragleave', function(e) {
+        e.target.style.borderTop = "none";
+    });
+
+    element.addEventListener('dragend', function(e) {
+        if (dragElem != lastDragElem && lastDragElem != null) {
+            dragElem.remove();
+            lastDragElem.before(dragElem);
+        }
+        dragElem.style.opacity = '1';
+        dragElem.style.height = '';
+        tempLi.style.display = 'none';
+        dragElem = null;
+        lastDragElem = null;
+    });
+});
