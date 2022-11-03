e6dbc1ff35ba976247e4239562c21e140023487a
Clément <clement.delord@epitech.eu>
2022-11-03 10:54:43 +0100

    cours events + ex 10

---
+++ Exercices/Exercice 10/consigne.md
@@ -0,0 +1,3 @@
+ajouter une vérification sur le champ email en js
+grâce à une regex, vérifiez qu'un email a bien été renseigné,
+et qu'il est bien formatté.
---
+++ Exercices/Exercice 10/script.js
@@ -0,0 +1,13 @@
+let form = document.querySelector('form');
+let input = document.querySelector('input');
+
+form.addEventListener('submit', function (e) {
+    //prevenir l'envoi du formulaire
+    e.preventDefault();
+
+    //vérifier si le champ est vide ou bien formatté
+    if (input.value == '' || input.value.match(/^(0|\+33)(6|7)[0-9]{8}$/)) {
+        form.submit();
+    }
+
+});
