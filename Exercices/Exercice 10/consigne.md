1fb076c1c73b36893a46e2295a80b0457fcdf055
Clément <clement.delord@epitech.eu>
2022-11-03 11:10:14 +0100

    ex 10 suite

--- Exercices/Exercice 10/consigne.md
+++ Exercices/Exercice 10/consigne.md
@@ -1,3 +1,6 @@
 ajouter une vérification sur le champ email en js
 grâce à une regex, vérifiez qu'un email a bien été renseigné,
 et qu'il est bien formatté.
+
+
+Mettre en valeur les champs qui génèrent une erreur lors du blocage de l'envoi.
--- Exercices/Exercice 10/script.js
+++ Exercices/Exercice 10/script.js
@@ -1,13 +1,15 @@
 let form = document.querySelector('form');
 let input = document.querySelector('input');
+let inputMail = document.getElementById('mail');
 
 form.addEventListener('submit', function (e) {
     //prevenir l'envoi du formulaire
     e.preventDefault();
 
     //vérifier si le champ est vide ou bien formatté
-    if (input.value == '' || input.value.match(/^(0|\+33)(6|7)[0-9]{8}$/)) {
-        form.submit();
+    if (input.value.match(/^(0|\+33)(6|7)[0-9]{8}$/)) {
+        if (inputMail.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+            form.submit();
+        }
     }
-
 });
